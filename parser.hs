bca39ab90183cca90a3b26c198dfb85b0f62059e
Matthieu Martin <ma.tt.b.ma.rt.in+parquet@gmail.com>
2015-11-18 15:29:45 -0800

    initial version

---
+++ parser.hs
@@ -0,0 +1,70 @@
+module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse spacesPlusString "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value " ++ show val
+
+spacesPlusString :: Parser LispVal
+spacesPlusString = do
+                     spaces
+		     parseExpr
+
+main :: IO ()
+main = do 
+         expr <- getLine
+         putStrLn (readExpr expr)
+
+spaces = skipMany1 space 
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+	     deriving (Show)
+ 
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many parseStringChar
+                char '"'
+                return $ String x
+
+parseStringChar = (parseEscapedChar <|> noneOf "\"")
+
+parseEscapedChar = do
+                      char '\\'
+                      oneOf "\""
+                      return '"' 
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of 
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+parseNumber :: Parser LispVal
+--parseNumber = liftM (Number . read) $ many1 digit
+--parseNumber = do 
+--              x <- many1 digit 
+--              return  (Number  (read x))
+parseNumber = many1 digit >>=
+                return . Number . read
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
